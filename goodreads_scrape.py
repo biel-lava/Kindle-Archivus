9a0af2e65cd1c86efb00b8ddc7e4de0d5012f232
Biel Lava <ralava@up.edu.ph>
2024-12-10 22:36:47 +0800

    initial testing of scrape functions

--- goodreads_scrape.py
+++ goodreads_scrape.py
@@ -2,3 +2,20 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import re 
+
+# create soup object from goodreads url
+def create_book_data(url):
+    # extract the html content of the webpage
+    goodreads_url = url
+    book_page = requests.get(goodreads_url)
+
+    # extract html content from the response object
+    html_content = book_page.content
+    book_data = BeautifulSoup(html_content, 'html.parser')
+
+    # return the book data for scraping
+    return book_data
+
+
+def book_title():
+    return None
